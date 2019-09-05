2973cd9868a4eea80fd6057c599a0eec034dd2a0
vandna kapoor <vandnakapoor19@gmail.com>
2019-09-05 11:50:59 -0400

    implemented basic map using mapbox API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import 'mapbox-gl/dist/mapbox-gl.css'
+import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
+import MapGL, {NavigationControl} from 'react-map-gl';
+import Geocoder from 'react-map-gl-geocoder';
+const TOKEN = 'pk.eyJ1IjoidmFuZG5ha2Fwb29yIiwiYSI6ImNqemVrZWd1ejAwMHczbnBoN281ZGk5bmwifQ.5Zu--NifaKp_tSQDR9YdRw';
+const navStyle = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  padding: '10px'
+};
+export default class Map extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+constructor(props) {
+    super(props);
+    var geocoder = new Geocoder({ accessToken: TOKEN });
+    this.state = {
+      viewport: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      latitude: 37.7577,
+      longitude: -122.4376,
+      zoom: 8
+      },
+    };
+  }
+ mapRef = React.createRef()
+ 
+  handleViewportChange = (viewport) => {
+    this.setState({
+      viewport: { ...this.state.viewport, ...viewport }
+    })
+  }
+   // if you are happy with Geocoder default settings, you can just use handleViewportChange directly
+   handleGeocoderViewportChange = (viewport) => {
+    const geocoderDefaultOverrides = { transitionDuration: 1000 }
+ 
+    return this.handleViewportChange({
+      ...viewport,
+      ...geocoderDefaultOverrides
+    })
+  }
+render() {
+    const {viewport} = this.state;
+    console.log(viewport);
+return (
+      <MapGL
+        ref={this.mapRef}
+        {...viewport}
+        mapStyle="mapbox://styles/denizzed/cjyu2kr5e0rqq1cry6wmo3rhg"
+        mapboxApiAccessToken={TOKEN} 
+        onViewportChange={this.handleViewportChange}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <div className="nav" style={navStyle}>
+          <NavigationControl/>
+        </div>
+        <Geocoder
+          mapRef={this.mapRef}
+          onViewportChange={this.handleGeocoderViewportChange}
+          mapboxApiAccessToken={TOKEN}
+        />
+      </MapGL>
+    );
+  }
 }
-
-export default App;
